adf4f330b2c8dd77193dcda94c92af104f9f96c6
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-14 08:42:00 +0100

    Route for new message's form

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Form for new messages */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Form for new message');
 });
 
 module.exports = router;
