964efc441b8973aa0141a28a4362093a18cd38cc
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-14 10:36:46 +0100

    Clean code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const messages = require('../public/javascripts/messages')
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages });
 });
 
 module.exports = router;
